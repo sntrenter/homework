a5a3d7a665b13fa2d8ee6660f76b61f5851aec00
STrenter <sntrenter@gmail.com>
2019-01-24 11:57:42 -0600

    added exit button and fixed picture issue

--- DOTNET/trenter_a1/trenter_a1/Form1.Designer.cs
+++ DOTNET/trenter_a1/trenter_a1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.aboutMe = new System.Windows.Forms.TextBox();
             this.selfPorterait = new System.Windows.Forms.PictureBox();
+            this.exitButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.selfPorterait)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,23 +42,34 @@
             this.aboutMe.Name = "aboutMe";
             this.aboutMe.Size = new System.Drawing.Size(185, 194);
             this.aboutMe.TabIndex = 0;
-            this.aboutMe.Text = resources.GetString("aboutMe.Text");
             // 
             // selfPorterait
             // 
             this.selfPorterait.Image = ((System.Drawing.Image)(resources.GetObject("selfPorterait.Image")));
-            this.selfPorterait.ImageLocation = "strenter.jpg";
+            this.selfPorterait.ImageLocation = "";
             this.selfPorterait.Location = new System.Drawing.Point(12, 32);
             this.selfPorterait.Name = "selfPorterait";
             this.selfPorterait.Size = new System.Drawing.Size(202, 201);
             this.selfPorterait.TabIndex = 1;
             this.selfPorterait.TabStop = false;
+            this.selfPorterait.Click += new System.EventHandler(this.selfPorterait_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(323, 441);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 452);
+            this.ClientSize = new System.Drawing.Size(427, 519);
+            this.Controls.Add(this.exitButton);
             this.Controls.Add(this.selfPorterait);
             this.Controls.Add(this.aboutMe);
             this.Name = "Form1";
@@ -72,6 +84,7 @@
 
         private System.Windows.Forms.TextBox aboutMe;
         private System.Windows.Forms.PictureBox selfPorterait;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- DOTNET/trenter_a1/trenter_a1/Form1.cs
+++ DOTNET/trenter_a1/trenter_a1/Form1.cs
@@ -17,5 +17,15 @@ namespace trenter_a1
             InitializeComponent();
         }
 
+        private void selfPorterait_Click(object sender, EventArgs e)
+        {
+            var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+            aboutMe.Text = text;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
     }
 }
