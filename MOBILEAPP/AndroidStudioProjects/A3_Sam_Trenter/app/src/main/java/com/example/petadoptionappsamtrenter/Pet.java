151d1af99ec2918023a3ac8a3204b91eaddb5422
Trenter <snt433@live.missouristate.edu>
2019-03-02 12:30:45 -0600

    u fuked up the commit

--- MOBILEAPP/AndroidStudioProjects/A3_Sam_Trenter/app/src/main/java/com/example/petadoptionappsamtrenter/Pet.java
+++ MOBILEAPP/AndroidStudioProjects/A3_Sam_Trenter/app/src/main/java/com/example/petadoptionappsamtrenter/Pet.java
@@ -1,4 +1,19 @@
 package com.example.petadoptionappsamtrenter;
 
 public class Pet {
+
+// Declare constants for all six combinations of pets and fee types
+
+
+// Declare private fields for the adoption fee, microchip fee, spaying and neutering fee, and total fees
+
+
+// Add a constructor method to initialize all field (member variable) values to 0
+
+
+// Add three setter methods to set each fee type
+
+
+// Add one getter method to return the total fees
+
 }
