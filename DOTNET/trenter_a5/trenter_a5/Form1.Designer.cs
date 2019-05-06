ff1a08ac108f699deaefa89298df5a8f994f84ed
LAPTOP-SR3771M1\sntrenter <sntrenter@gmail.com>
2019-05-06 15:49:00 -0600

    a5 completed

---
+++ DOTNET/trenter_a5/trenter_a5/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace trenter_a5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number
+            // 
+            this.number.Location = new System.Drawing.Point(320, 101);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(100, 22);
+            this.number.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of random numbers (0-100):";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Open File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(99, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 404);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number);
+            this.Name = "Form1";
+            this.Text = "Random Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox number;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DOTNET/trenter_a5/trenter_a5/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace trenter_a5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        String generateRand()
+        {
+            Random rand = new Random();
+            String temp = number.Text;
+            int repeats;
+            int.TryParse(temp, out repeats);
+            repeats = repeats - 1;
+            //MessageBox.Show(repeats.ToString());
+            List<String> numberMessage = new List<string> { "", "", "", "", "" };
+            for(int i = 0;i <= repeats ;i++ )
+            {
+                int num = rand.Next(101);
+                //MessageBox.Show(num.ToString());
+                if (num < 20)      { numberMessage[0] += " " + num.ToString()+","; }
+                else if (num < 40) { numberMessage[1] += " " + num.ToString() + ","; }
+                else if (num < 60) { numberMessage[2] += " " + num.ToString() + ","; }
+                else if (num < 80) { numberMessage[3] += " " + num.ToString() + ","; }
+                else if (num < 100){ numberMessage[4] += " " + num.ToString() + ","; }
+            }
+
+            String fullString = "Here are your numbers: \n\r" +
+               "0-19:" + numberMessage[0] + "\n\r" +
+               "20-39:" + numberMessage[1] + "\n\r" +
+               "40-59:" + numberMessage[2] + "\n\r" +
+               "60-79:" + numberMessage[3] + "\n\r" +
+               "80-100:" + numberMessage[4];
+
+            MessageBox.Show(fullString);
+            return fullString;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String x = generateRand();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //Save File
+            //if (File.Exists("numbers.txt")) { File.Delete("numbers.txt"); }
+            StreamWriter outputFile;
+            outputFile = File.CreateText("numbers.txt");
+            String x = generateRand();
+            outputFile.Write(x);
+            outputFile.Close();
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //Open File
+            //these two lines are the cool way to do it
+            //if (File.Exists("numbers.txt")){ System.Diagnostics.Process.Start("numbers.txt"); }
+            //else { MessageBox.Show("the file didn't exits"); }
+            StreamReader reader = new StreamReader("numbers.txt");
+            String text = reader.ReadToEnd();
+            MessageBox.Show(text);
+            reader.Close();
+            
+        }
+    }
+}
