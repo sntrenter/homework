55fcfff055d54a3369ac248d23e3e8e0bc7d1162
LAPTOP-SR3771M1\sntrenter <sntrenter@gmail.com>
2019-05-06 23:27:44 -0600

    finished a6

---
+++ DOTNET/trenter_a6/trenter_a6/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace trenter_a6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(87, 236);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(87, 311);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "F to C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Show All Divisors";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 43);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Leap Year";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(243, 309);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 75);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Show Leap Years in the next N Years";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 454);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Methods Examples";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ DOTNET/trenter_a6/trenter_a6/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trenter_a6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FtoC();
+        }
+        void FtoC()
+        {
+            String F = textBox1.Text;
+            double Fnum;
+            double.TryParse(F, out Fnum);
+            //MessageBox.Show(Fnum.ToString());
+            //(Fnum - 32) * (5 / 9);
+            MessageBox.Show(F + " degrees F is equal to " +
+               ((Fnum - 32.00) * (5.00 / 9.00)).ToString("###.##") + " degrees Celsius");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            ShowDivisors();
+        }
+        void ShowDivisors()
+        {
+            String snum = textBox2.Text;
+            int num;
+            int.TryParse(snum, out num);
+            String S = "The factors of "+snum+" are: ";
+            for(int i = 1;i<num+1 ; i++)
+            {
+                //MessageBox.Show((num % i).ToString());
+                if (num % i == 0) { S += (i).ToString() + ","; }
+            }
+            MessageBox.Show(S);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (isLeapYear(Int32.Parse(textBox3.Text.ToString()))){ MessageBox.Show(textBox3.Text+" is a leap year!"); }
+            else { MessageBox.Show(textBox3.Text + " is not leap year!"); }
+        }
+        bool isLeapYear(int year)
+        {
+            return DateTime.IsLeapYear(year);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Nyears();
+        }
+        void Nyears()
+        {
+            int date = DateTime.Now.Year;
+            int year = Int32.Parse(textBox4.Text);
+            String S = "Leap years in the next " + year.ToString() + " are: ";
+            for (int i = date; i < date + year; i++)
+            {
+                if (isLeapYear(i)) { S += i.ToString() + ","; }
+            }
+            MessageBox.Show(S);
+        }
+    }
+}
